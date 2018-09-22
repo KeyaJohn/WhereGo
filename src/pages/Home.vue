fb916978e74215a6a93a3232fef7821b85dd4a5f
tikaijing <1572831416@qq.com>
2018-09-22 08:36:08 +0800

    add swiper

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <Header></Header>
+    <Swiper></Swiper>
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Swiper from './components/Swiper'
 export default {
   name: 'Home',
   components: {
-    Header: Header
+    Header: Header,
+    Swiper: Swiper
   }
 }
 </script>
---
+++ src/pages/components/Swiper.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(slide, index) in swiperSlides" :key="index">
+        <img class="swiper-img" v-bind:src="slide"/>
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'scarrousel',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true,
+        autoplay: 4000, //  可选选项，自动滑动
+        autoplayDisableOnInteraction: false // 注意此参数，默认为true
+      },
+      swiperSlides: ['https://img1.qunarzz.com/vc/f3/ea/ae/095dd61c09f2797ba9147bd03f.jpg', 'https://img1.qunarzz.com/vc/4e/c2/9a/61222b895136533c133ee9e8bc.jpg', 'https://img1.qunarzz.com/vc/1f/ba/ac/13c3552a1cb338991301f9b0ad.jpg', 'https://img1.qunarzz.com/vc/ce/4b/45/7f78e2003fb5fb65c57eb1a800.jpg']
+    }
+  },
+  mounted () {
+    setInterval(() => {
+      console.log('simulate async data')
+      if (this.swiperSlides.length < 4) {
+        this.swiperSlides.push(this.swiperSlides.length + 1)
+      }
+    }, 3000)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: red
+  .swiper-img
+    width: 100%
+</style>
